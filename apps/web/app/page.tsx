1f40cc8628f37ee59f54a19edd08d6207eab5e7e
Tashila-Kumara <tashila.kumara@1billiontech.com>
2026-06-23 12:43:06 -0700

    feat(auth): authentication
    clerkjs

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>HEello world</div>;
+// export default function Home() {
+//   return <div>HEello world</div>;
+// }
+
+import { SignInButton } from "@clerk/nextjs";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+
+      <SignInButton mode="modal">
+        <button>Login</button>
+      </SignInButton>
+    </div>
+  );
 }
