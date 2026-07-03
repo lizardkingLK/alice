a55c29e16635ba843dd8e5cfd67ce66e2dbcd60e
Tashila-Kumara <tashila.kumara@1billiontech.com>
2026-07-03 14:53:26 +0530

    feat: folder structured

--- apps/web/app/admin/layout.tsx
+++ apps/web/app/admin/layout.tsx
@@ -1,16 +1,4 @@
-import { Metadata } from 'next';
-import React from 'react';
+import AdminLayout, { metadata } from '@/components/admin/layout';
 
-export const metadata: Metadata = {
-  title: 'Admin',
-  robots: {
-    index: false,
-    follow: false,
-  },
-};
-
-export default function AdminLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <section>{children}</section>;
-}
+export { metadata };
+export default AdminLayout;
